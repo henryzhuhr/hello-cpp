502dbb49b22048327688678c0573f5367bd7fb57
Henry_Zhu <38650110+HenryZhuHR@users.noreply.github.com>
2021-10-26 23:19:52 +0800

    happy c

--- build.bat
+++ build.bat
@@ -1,33 +1,18 @@
 @REM @echo off
 
-set HOME=%cd%
-
-@REM set CMAKE_BIN=%HOME%/tools/cmake-3.20.1-windows-x86_64/bin/
-@REM set path=%PATH%;%CMAKE_BIN%
-
-@REM set MinGW64_BIN=%HOME%/tools/mingw64/bin/
-@REM set path=%PATH%;%MinGW64_BIN%
-
-
-
 rmdir /s /q bin
-rmdir /s /q build
-
+@REM rmdir /s /q build
+del "build\CMakeCache.txt"
 
 mkdir build
 cd build
 cmake ^
-    -G "MinGW Makefiles" ^
     -DCMAKE_BUILD_TYPE=Release ^
     ..   
 @REM cmake .. -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release
 make -j8
 
-
-cd ../
-
-rmdir /s /q build
-
-cd bin
+cd ../bin
 memory_deep_copy.exe
 
+@REM rmdir /s /q "..\build"
