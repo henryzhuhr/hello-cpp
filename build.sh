231b38484410f9e9652d36fc774eac7422e1898c
Henry Zhu <38650110+HenryZhuHR@users.noreply.github.com>
2022-03-03 11:06:27 +0800

    📝docs: algorithm dynamic programming

--- build.sh
+++ build.sh
@@ -10,14 +10,19 @@ make -j8
 cd ..
 
 
+
+# --- RUN:: MAIN ---
 cd bin
 # ./main
 # ./test-opencv "../images/lena.jpg"
-./lvalue_rvalue
+# ./lvalue_rvalue
 cd ..
 
-# run STL
+# --- RUN:: STL ---
 # cd bin/stl
 # ./stl_vector
 
-
+# --- RUN:: algorithm ---
+cd bin/algorithm
+./dynamic_programming
+cd ../..
