7de4426912b6e3ed499d969d8c7322822fdcb256
Henry Zhu <38650110+HenryZhuHR@users.noreply.github.com>
2022-03-16 22:16:40 +0800

    git

---
+++ git.sh
@@ -0,0 +1,14 @@
+# --- add ---
+# git add <filename>
+git add *
+git commit -m "代码提交信息"
+
+
+# --- pull ---
+# git merge <branch>
+git pull
+git merge main
+
+
+# --- push ---
+git push origin master
